570aca6ee3801fb8306887689b620432331640c2
ViniciosSantos25 <viniciossantos25@gmail.com>
2023-06-22 18:24:59 -0300

    Criação do botão show

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//<!--CSS BOOTSTRAP 5-->
-//    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+function show(el){
+    const display = document.getElementById(el).style.display;
+    if(display == 'none'){
+        document.getElementById(el).style.display = 'block';
+    }else{
+        document.getElementById(el).style.display = 'none';
+    }
+}//função para esconder e mostrar o conteudo das "areas" de informação
